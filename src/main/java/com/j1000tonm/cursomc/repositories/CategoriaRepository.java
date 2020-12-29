853de1ce9373a184e84a0a71406ef7f9469cd374
José Milton <j1000tonm.jmb@gmail.com>
2020-12-29 15:07:20 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/j1000tonm/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.j1000tonm.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.j1000tonm.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
