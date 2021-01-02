d94c8462a96c6ab8f4990804b59b76160d0b6d54
José Milton <j1000tonm.jmb@gmail.com>
2021-01-01 23:58:25 -0300

    EndPoint /Pedidos/{id} disponibilizado

--- src/main/java/com/j1000tonm/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/j1000tonm/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.j1000tonm.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
